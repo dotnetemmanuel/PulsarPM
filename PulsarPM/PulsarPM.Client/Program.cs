6d748408254b6e398884595cee50b23c7736dd1c
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-12-09 10:23:54 +0100

    fixed a good amount of sonarqube warnings

--- PulsarPM/PulsarPM.Client/Program.cs
+++ PulsarPM/PulsarPM.Client/Program.cs
@@ -16,7 +16,6 @@ builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticat
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(new Uri(builder.HostEnvironment.BaseAddress), "api/") });
 
-// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProjectService, ProjectService>();
 builder.Services.AddScoped<ICardService, CardService>();
 builder.Services.AddSingleton<ProjectStateService>();
