f23f369e7f8733530d08943d9b45c3f128b9bdb3
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2024-12-10 15:43:45 +0100

    added test of dropzonechange

--- PulsarPM.Tests/UnitTests/CardServiceTests.cs
+++ PulsarPM.Tests/UnitTests/CardServiceTests.cs
@@ -8,51 +8,53 @@ using Shared;
 
 namespace PulsarPM.Tests;
 
+using MudBlazor;
+
 public class CardServiceTests
 {
-    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
-    private readonly HttpClient _httpClient;
-    private readonly ICardService _cardService;
-    private readonly ILogger<CardService> _logger;
+  private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+  private readonly HttpClient _httpClient;
+  private readonly ICardService _cardService;
+  private readonly ILogger<CardService> _logger;
 
-    public CardServiceTests()
+  public CardServiceTests()
+  {
+    _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+    _httpClient = new HttpClient(_mockHttpMessageHandler.Object)
     {
-        _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+      BaseAddress = new Uri("http://localhost/api/")
+    };
 
-        _httpClient = new HttpClient(_mockHttpMessageHandler.Object)
-        {
-            BaseAddress = new Uri("http://localhost/api/")
-        };
+    _logger = Mock.Of<ILogger<CardService>>();
+    _cardService = new CardService(_httpClient, _logger);
+  }
 
-        _logger = Mock.Of<ILogger<CardService>>();
-        _cardService = new CardService(_httpClient, _logger);
-    }
-
-    [Fact]
-    public async Task CreateCardAsync_ShouldReturnCreatedCardDTO()
+  [Fact]
+  public async Task CreateCardAsync_ShouldReturnCreatedCardDTO()
+  {
+    // Arrange
+    var newCard = new CardDTO
     {
-        // Arrange
-        var newCard = new CardDTO
-        {
-            Id = 1,
-            Name = "Test Card",
-            Description = "Test Description",
-            Status = "Backlog",
-            Color = "Blue",
-            ProjectId = 1,
-            Order = 0
-        };
+      Id = 1,
+      Name = "Test Card",
+      Description = "Test Description",
+      Status = "Backlog",
+      Color = "Blue",
+      ProjectId = 1,
+      Order = 0
+    };
 
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(@"{
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"{
                     ""id"": 1,
                     ""name"": ""Test Card"",
                     ""description"": ""Test Description"",
@@ -62,71 +64,71 @@ public class CardServiceTests
                     ""order"": 0
                    
                 }")
-            });
+      });
 
-        // Act
-        var result = await _cardService.CreateCardAsync(newCard);
+    // Act
+    var result = await _cardService.CreateCardAsync(newCard);
 
 
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(newCard.Id, result.Id);
-        Assert.Equal(newCard.Name, result.Name);
-        Assert.Equal(newCard.Description, result.Description);
-        Assert.Equal(newCard.Status, result.Status);
-        Assert.Equal(newCard.Color, result.Color);
-        Assert.Equal(newCard.ProjectId, result.ProjectId);
-        Assert.Equal(newCard.Order, result.Order);
-    }
+    // Assert
+    Assert.NotNull(result);
+    Assert.Equal(newCard.Id, result.Id);
+    Assert.Equal(newCard.Name, result.Name);
+    Assert.Equal(newCard.Description, result.Description);
+    Assert.Equal(newCard.Status, result.Status);
+    Assert.Equal(newCard.Color, result.Color);
+    Assert.Equal(newCard.ProjectId, result.ProjectId);
+    Assert.Equal(newCard.Order, result.Order);
+  }
 
-    [Fact]
-    public async Task DeleteCardAsync_ShouldReturnTrue()
+  [Fact]
+  public async Task DeleteCardAsync_ShouldReturnTrue()
+  {
+    var cardToDelete = new CardDTO
     {
-        var cardToDelete = new CardDTO
-        {
-            Id = 1,
-            Name = "Test Card",
-            Description = "Test Description",
-            Status = "Backlog",
-            Color = "Blue",
-            ProjectId = 1,
-            Order = 0
-        };
+      Id = 1,
+      Name = "Test Card",
+      Description = "Test Description",
+      Status = "Backlog",
+      Color = "Blue",
+      ProjectId = 1,
+      Order = 0
+    };
 
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK
-            });
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK
+      });
 
-        // Act
-        await _cardService.DeleteCardAsync(cardToDelete);
+    // Act
+    await _cardService.DeleteCardAsync(cardToDelete);
 
-        //Assert
-        Assert.True(true);
-    }
+    //Assert
+    Assert.True(true);
+  }
 
-    [Fact]
-    public async Task GetCardsAsync_ShouldReturnAllCards()
-    {
-        //Arrange
-        int projectId = 1;
+  [Fact]
+  public async Task GetCardsAsync_ShouldReturnAllCards()
+  {
+    //Arrange
+    int projectId = 1;
 
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(@"[{
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"[{
                     ""id"": 1,
                     ""name"": ""Test Card"",
                     ""description"": ""Test Description"",
@@ -145,57 +147,57 @@ public class CardServiceTests
                     ""projectId"": 1,
                     ""order"": 1
                 }]")
-            });
+      });
 
-        //Act
-        var result = await _cardService.GetCardsFromProjectAsync(projectId);
+    //Act
+    var result = await _cardService.GetCardsFromProjectAsync(projectId);
 
-        //Assert
-        Assert.NotNull(result);
-        Assert.NotEmpty(result);
-        Assert.Equal(2, result.Count);
-        //card 1
-        Assert.Equal("Test Card", result[0].Name);
-        Assert.Equal("Test Description", result[0].Description);
-        Assert.Equal("Backlog", result[0].Status);
-        Assert.Equal("Blue", result[0].Color);
-        Assert.Equal(projectId, result[0].ProjectId);
-        Assert.Equal(0, result[0].Order);
+    //Assert
+    Assert.NotNull(result);
+    Assert.NotEmpty(result);
+    Assert.Equal(2, result.Count);
+    //card 1
+    Assert.Equal("Test Card", result[0].Name);
+    Assert.Equal("Test Description", result[0].Description);
+    Assert.Equal("Backlog", result[0].Status);
+    Assert.Equal("Blue", result[0].Color);
+    Assert.Equal(projectId, result[0].ProjectId);
+    Assert.Equal(0, result[0].Order);
 
-        //card 2
-        Assert.Equal("Test Card2", result[1].Name);
-        Assert.Equal("Test Description2", result[1].Description);
-        Assert.Equal("Backlog", result[1].Status);
-        Assert.Equal("Green", result[1].Color);
-        Assert.Equal(projectId, result[1].ProjectId);
-        Assert.Equal(1, result[1].Order);
-    }
-    
-    [Fact]
-    public async Task UpdateCardAsync_ShouldReturnUpdatedCardDTO()
+    //card 2
+    Assert.Equal("Test Card2", result[1].Name);
+    Assert.Equal("Test Description2", result[1].Description);
+    Assert.Equal("Backlog", result[1].Status);
+    Assert.Equal("Green", result[1].Color);
+    Assert.Equal(projectId, result[1].ProjectId);
+    Assert.Equal(1, result[1].Order);
+  }
+
+  [Fact]
+  public async Task UpdateCardAsync_ShouldReturnUpdatedCardDTO()
+  {
+    // Arrange
+    var updatedCard = new CardDTO
     {
-        // Arrange
-        var updatedCard = new CardDTO
-        {
-            Id = 1,
-            Name = "Test Card",
-            Description = "Test Description",
-            Status = "Backlog",
-            Color = "Blue",
-            ProjectId = 1,
-            Order = 0
-        };
+      Id = 1,
+      Name = "Test Card",
+      Description = "Test Description",
+      Status = "Backlog",
+      Color = "Blue",
+      ProjectId = 1,
+      Order = 0
+    };
 
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(@"{
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"{
                     ""id"": 1,
                     ""name"": ""Test Card"",
                     ""description"": ""Test Description"",
@@ -205,19 +207,83 @@ public class CardServiceTests
                     ""order"": 0
                    
                 }")
-            });
+      });
 
-        // Act
-        var result = await _cardService.UpdateCardAsync(updatedCard);
+    // Act
+    var result = await _cardService.UpdateCardAsync(updatedCard);
 
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(updatedCard.Id, result.Id);
-        Assert.Equal(updatedCard.Name, result.Name);
-        Assert.Equal(updatedCard.Description, result.Description);
-        Assert.Equal(updatedCard.Status, result.Status);
-        Assert.Equal(updatedCard.Color, result.Color);
-        Assert.Equal(updatedCard.ProjectId, result.ProjectId);
-        Assert.Equal(updatedCard.Order, result.Order);
-    }
+    // Assert
+    Assert.NotNull(result);
+    Assert.Equal(updatedCard.Id, result.Id);
+    Assert.Equal(updatedCard.Name, result.Name);
+    Assert.Equal(updatedCard.Description, result.Description);
+    Assert.Equal(updatedCard.Status, result.Status);
+    Assert.Equal(updatedCard.Color, result.Color);
+    Assert.Equal(updatedCard.ProjectId, result.ProjectId);
+    Assert.Equal(updatedCard.Order, result.Order);
+  }
+
+  [Fact]
+  public async Task EditCardStatus_ShouldReturnUpdatedStatus()
+  {
+    //Arrange
+    ////Simulate Dropzones
+
+    var card = new CardDTO
+    {
+      Name = "Hello",
+      Description = "How are you today?",
+      Status = "Backlog",
+      Color = "Blue",
+      ProjectId = 1,
+      Order = 0
+    };
+
+    MudItemDropInfo<CardDTO>? mudItemDropInfo = new(card, "Backlog", 0);
+    MudItemDropInfo<CardDTO>? mudItemDropInfo2 = new(null, "Done", 0);
+
+    _mockHttpMessageHandler.Protected()
+      .SetupSequence<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"{
+                    ""id"": 1,
+                    ""name"": ""Hello"",
+                    ""description"": ""How are you today?"",
+                    ""status"": ""Backlog"",
+                    ""color"": ""Blue"",
+                    ""projectId"": 1,
+                    ""order"": 0                   
+                }")
+      })
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"{
+                ""id"": 1,
+                ""name"": ""Hello"",
+                ""description"": ""How are you today?"",
+                ""status"": ""Done"",
+                ""color"": ""Blue"",
+                ""projectId"": 1,
+                ""order"": 0                   
+            }")
+      });
+
+    await _cardService.CreateCardAsync(card);
+
+    //Act
+    //Satus change, card is moved to zone nr2.
+    card.Status = mudItemDropInfo2.DropzoneIdentifier;
+    var result = await _cardService.UpdateCardAsync(card);
+
+    //Assert
+    Assert.NotNull(result);
+    Assert.Equal(card.Status, result.Status);
+  }
 }
--- PulsarPM.Tests/UnitTests/ProjectServiceTests.cs
+++ PulsarPM.Tests/UnitTests/ProjectServiceTests.cs
@@ -9,39 +9,39 @@ using Shared;
 
 public class ProjectServiceTests
 {
-    private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
-    private readonly HttpClient _httpClient;
-    private readonly IProjectService _projectService;
-    private readonly ILogger<ProjectService> _logger;
+  private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+  private readonly HttpClient _httpClient;
+  private readonly IProjectService _projectService;
+  private readonly ILogger<ProjectService> _logger;
 
-    //run all tests Ctrl+U Ctrl+L
-    public ProjectServiceTests()
+  //run all tests Ctrl+U Ctrl+L
+  public ProjectServiceTests()
+  {
+    _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+
+    _httpClient = new HttpClient(_mockHttpMessageHandler.Object)
     {
-        _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+      BaseAddress = new Uri("http://localhost/api/")
+    };
 
-        _httpClient = new HttpClient(_mockHttpMessageHandler.Object)
-        {
-            BaseAddress = new Uri("http://localhost/api/")
-        };
+    _logger = Mock.Of<ILogger<ProjectService>>();
+    _projectService = new ProjectService(_httpClient, _logger);
+  }
 
-        _logger = Mock.Of<ILogger<ProjectService>>();
-        _projectService = new ProjectService(_httpClient, _logger);
-    }
-
-    [Fact]
-    public async Task GetProjectsAsync_ShouldReturnListOfProjects()
-    {
-        //Arrange
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(@"[{
+  [Fact]
+  public async Task GetProjectsAsync_ShouldReturnListOfProjects()
+  {
+    //Arrange
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"[{
                     ""id"": 1,
                     ""name"": ""Test Project"",
                     ""cards"": [],
@@ -53,125 +53,125 @@ public class ProjectServiceTests
                     ""cards"": [],
                     ""isArchived"": false
                 }]")
-            });
+      });
 
-        //Act
-        var result = await _projectService.GetProjectsAsync();
+    //Act
+    var result = await _projectService.GetProjectsAsync();
 
-        //Assert
-        Assert.NotNull(result);
-   
-        //project 1
-        Assert.Equal(1, result[0].Id);
-        Assert.Equal("Test Project", result[0].Name);
-        Assert.Empty(result[0].Cards);
-        Assert.False(result[0].IsArchived);
-        
-        //project 2
-        Assert.Equal(2, result[1].Id);
-        Assert.Equal("Test Project2", result[1].Name);
-        Assert.Empty(result[1].Cards);
-        Assert.False(result[1].IsArchived);
-    }
+    //Assert
+    Assert.NotNull(result);
 
-    [Fact]
-    public async Task GetProjectByIdAsync_ShouldReturnProjectDTO()
-    {
-        //Arrange
-        int projectId = 1;
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(@"{
+    //project 1
+    Assert.Equal(1, result[0].Id);
+    Assert.Equal("Test Project", result[0].Name);
+    Assert.Empty(result[0].Cards);
+    Assert.False(result[0].IsArchived);
+
+    //project 2
+    Assert.Equal(2, result[1].Id);
+    Assert.Equal("Test Project2", result[1].Name);
+    Assert.Empty(result[1].Cards);
+    Assert.False(result[1].IsArchived);
+  }
+
+  [Fact]
+  public async Task GetProjectByIdAsync_ShouldReturnProjectDTO()
+  {
+    //Arrange
+    int projectId = 1;
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"{
                     ""id"": 1,
                     ""name"": ""Test Project"",
                     ""cards"": [],
                     ""isArchived"": false
                 }")
-            });
+      });
 
-        //Act
-        var result = await _projectService.GetProjectByIdAsync(projectId);
+    //Act
+    var result = await _projectService.GetProjectByIdAsync(projectId);
 
-        //Assert
-        Assert.NotNull(result);
-        Assert.Equal(projectId, result.Id);
-        Assert.Equal("Test Project", result.Name);
-        Assert.Empty(result.Cards);
-        Assert.False(result.IsArchived);
-    }
+    //Assert
+    Assert.NotNull(result);
+    Assert.Equal(projectId, result.Id);
+    Assert.Equal("Test Project", result.Name);
+    Assert.Empty(result.Cards);
+    Assert.False(result.IsArchived);
+  }
 
-    [Fact]
-    public async Task CreateProjectAsync_ShouldReturnCreatedProjectDTO()
+  [Fact]
+  public async Task CreateProjectAsync_ShouldReturnCreatedProjectDTO()
+  {
+    //Arrange
+    var newProject = new ProjectDTO
     {
-        //Arrange
-        var newProject = new ProjectDTO
-        {
-            Id = 1,
-            Name = "Test Project",
-            Cards = new List<CardDTO>(),
-            IsArchived = false
-        };
+      Id = 1,
+      Name = "Test Project",
+      Cards = new List<CardDTO>(),
+      IsArchived = false
+    };
 
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK,
-                Content = new StringContent(@"{
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK,
+        Content = new StringContent(@"{
                     ""id"": 1,
                     ""name"": ""Test Project"",
                     ""cards"": [],
                     ""isArchived"": false
                 }")
-            });
+      });
 
-        //Act
-        var result = await _projectService.CreateProjectAsync(newProject);
+    //Act
+    var result = await _projectService.CreateProjectAsync(newProject);
 
-        //Assert
-        Assert.NotNull(result);
-        Assert.Equal(newProject.Id, result.Id);
-        Assert.Equal(newProject.Name, result.Name);
-        Assert.Equal(newProject.Cards, result.Cards);
-        Assert.Equal(newProject.IsArchived, result.IsArchived);
-    }
+    //Assert
+    Assert.NotNull(result);
+    Assert.Equal(newProject.Id, result.Id);
+    Assert.Equal(newProject.Name, result.Name);
+    Assert.Equal(newProject.Cards, result.Cards);
+    Assert.Equal(newProject.IsArchived, result.IsArchived);
+  }
 
-    [Fact]
-    public async Task DeleteProjectAsync_ShouldReturnTrue() //TODO: add name suffix
+  [Fact]
+  public async Task DeleteProjectAsync_ShouldReturnTrue()//TODO: add name suffix
+  {
+    // Arrange
+    var projectToDelete = new ProjectDTO
     {
-        // Arrange
-        var projectToDelete = new ProjectDTO
-        {
-            Id = 1,
-            Name = "Project To Delete"
-        };
+      Id = 1,
+      Name = "Project To Delete"
+    };
 
-        _mockHttpMessageHandler.Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.IsAny<HttpRequestMessage>(),
-                ItExpr.IsAny<CancellationToken>()
-            )
-            .ReturnsAsync(new HttpResponseMessage
-            {
-                StatusCode = HttpStatusCode.OK
-            });
+    _mockHttpMessageHandler.Protected()
+      .Setup<Task<HttpResponseMessage>>(
+      "SendAsync",
+      ItExpr.IsAny<HttpRequestMessage>(),
+      ItExpr.IsAny<CancellationToken>()
+      )
+      .ReturnsAsync(new HttpResponseMessage
+      {
+        StatusCode = HttpStatusCode.OK
+      });
 
-        // Act
-        await _projectService.DeleteProjectAsync(projectToDelete);
+    // Act
+    await _projectService.DeleteProjectAsync(projectToDelete);
 
-        //Assert
-        Assert.True(true);
-    }
+    //Assert
+    Assert.True(true);
+  }
 }
